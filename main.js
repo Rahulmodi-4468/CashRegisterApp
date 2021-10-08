d5918a2b2fe00b1161a19fede1eb5613d34cad2c
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-10-08 23:28:27 +0530

    UPDATED

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-give");
 const checkbutton = document.querySelector("#check-button");
-const message= document.querySelector("#error-message")
+const message1= document.querySelector("#error-message1")
+const message2= document.querySelector("#error-message2")
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes =[2000,500,200,100,50,20,10,5,2,1]
 const nextButton = document.querySelector("#Next");
 
-document.getElementById('cash-give').style.visibility = 'hidden';
-document.getElementById('check-button').style.visibility = 'hidden';
-document.getElementById('cashg').style.visibility = 'hidden';
-document.getElementById('change-table').style.visibility = 'hidden';
+document.getElementById('cash-give').style.display = 'none';
+document.getElementById('check-button').style.display = 'none';
+document.getElementById('cashg').style.display = 'none';
+document.getElementById('change-table').style.display = 'none';
+document.getElementById('img-Bill1').style.display = 'none';
+document.getElementById('img-Bill2').style.display = 'none';
+document.getElementById('error-message1').style.display = 'none';
+document.getElementById('error-message2').style.display = 'none';
 
-
-nextButton.addEventListener("click",function displayFurther(){
-    if(isNumeric(billAmount.value)){
-        document.getElementById('cashg').style.visibility = 'visible';
-        document.getElementById('cash-give').style.visibility = 'visible';
-        document.getElementById('check-button').style.visibility = 'visible';
+nextButton.addEventListener("click",function displayFurther()
+{
+    if(isNumeric(billAmount.value))
+    {
+        if(billAmount.value > 0)
+        {
+            document.getElementById('img-Bill1').style.display = 'none';
+            document.getElementById('error-message1').style.display = 'none';
+            document.getElementById('cashg').style.display = 'block';
+            document.getElementById('cash-give').style.display = 'block';
+            document.getElementById('check-button').style.display = 'block';
+        }
+        else{
+            document.getElementById('error-message1').style.display ="block";
+            message1.innerText = "😡Why are you do that?";
+            document.getElementById('img-Bill1').style.display='block';
+        }
+     
     }
     else{
-        showMessage("Don't test us ..plzz enter numeric value");
+        document.getElementById('error-message1').style.display ="block";
+        message1.innerText = "😡You test my code?";
+        document.getElementById('img-Bill1').style.display = 'block';
     }
     
 });
@@ -30,28 +49,34 @@ function isNumeric(n) {
 
 
 checkbutton.addEventListener("click" ,function validateBillAndCashAmount(){
-    hideMessage();
-   
-        if(billAmount.value > 0)
+        hideMessage();
+        if(isNumeric(cashGiven.value))
         {
             if(cashGiven.value >= billAmount.value)
             {   
                 const amountToBeReturned = cashGiven.value - billAmount.value;
                 calculateChange(amountToBeReturned);
-                document.getElementById('change-table').style.visibility = 'visible';
+                document.getElementById('change-table').style.display = 'block';
             }
             else{
                 showMessage("Do you wanna wash plates?");
             }
+         
         }
-      
+        else{
+            document.getElementById('change-table').style.display="none";
+            document.getElementById('error-message2').style.display ="block";
+            message2.innerText = "😡why you do again?";
+            document.getElementById('img-Bill2').style.display = 'block';
+        }
+    
    
 });
 
 
-
 function hideMessage(){
-    message.style.display ="none";
+    message2.style.display ="none";
+    document.getElementById('img-Bill2').style.display = 'none';
 }
 function calculateChange(amountToBeReturned)
 {
@@ -62,6 +87,6 @@ function calculateChange(amountToBeReturned)
     }
 }
 function showMessage(msg){
-    message.style.display ="block";
-    message.innerText = msg;
+    message2.style.display ="block";
+    message2.innerText = msg;
 }
