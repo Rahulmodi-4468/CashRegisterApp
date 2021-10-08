06ab819644e13f147bab59c55513cc7e61c9b960
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-10-08 15:43:42 +0530

    Done Cash Register App
    with simple javascript

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CashRegisterApp 
  Manage the cash register
+ Program has currencies of Rs.1,5,10,20,100,500,2000.
+ User enters a bill amount say Rs.243.
+ User then enters a cash given say Rs.2000.
+ Now, help the user y telling how can he/she return the change to the customer with minimum number of notes?
+ 
---
+++ main.js
@@ -0,0 +1,53 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-give");
+const checkbutton = document.querySelector("#check-button");
+const message= document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const availableNotes =[2000,500,200,100,50,20,10,5,2,1]
+const nextButton = document.querySelector("#Next");
+
+document.getElementById('cash-give').style.visibility = 'hidden';
+document.getElementById('check-button').style.visibility = 'hidden';
+document.getElementById('cashg').style.visibility = 'hidden';
+document.getElementById('change-table').style.visibility = 'hidden';
+
+
+nextButton.addEventListener("click",function displayFurther(){
+    document.getElementById('cashg').style.visibility = 'visible';
+    document.getElementById('cash-give').style.visibility = 'visible';
+    document.getElementById('check-button').style.visibility = 'visible';
+});
+
+checkbutton.addEventListener("click" ,function validateBillAndCashAmount(){
+    hideMessage();
+    if(billAmount.value > 0){
+        if(cashGiven.value >= billAmount.value)
+        {   
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+            document.getElementById('change-table').style.visibility = 'visible';
+
+            
+        }else{
+            showMessage("Do you wanna wash plates?");
+        }
+    }else{
+        showMessage("Invalid Bill Amount");
+       }
+});
+
+function hideMessage(){
+    message.style.display ="none";
+}
+function calculateChange(amountToBeReturned)
+{
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned /  availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+function showMessage(msg){
+    message.style.display ="block";
+    message.innerText = msg;
+}
