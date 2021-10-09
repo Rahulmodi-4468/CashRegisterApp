5089c5c4d0901777f73488c189b9177467aeedad
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-10-09 17:47:25 +0530

    images added

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ const nextButton = document.querySelector("#Next");
 
 document.getElementById('cash-give').style.display = 'none';
 document.getElementById('check-button').style.display = 'none';
-document.getElementById('cashg').style.display = 'none';
+document.getElementById('cashlabel').style.display = 'none';
 document.getElementById('change-table').style.display = 'none';
-document.getElementById('img-Bill1').style.display = 'none';
-document.getElementById('img-Bill2').style.display = 'none';
+document.getElementById('img-err1').style.display = 'none';
+document.getElementById('img-err2').style.display = 'none';
+document.getElementById('img-err3').style.display = 'none';
 document.getElementById('error-message1').style.display = 'none';
 document.getElementById('error-message2').style.display = 'none';
 
@@ -22,23 +23,19 @@ nextButton.addEventListener("click",function displayFurther()
     {
         if(billAmount.value > 0)
         {
-            document.getElementById('img-Bill1').style.display = 'none';
+            document.getElementById('img-err1').style.display = 'none';
             document.getElementById('error-message1').style.display = 'none';
-            document.getElementById('cashg').style.display = 'block';
+            document.getElementById('cashlabel').style.display = 'block';
             document.getElementById('cash-give').style.display = 'block';
             document.getElementById('check-button').style.display = 'block';
         }
         else{
-            document.getElementById('error-message1').style.display ="block";
-            message1.innerText = "😡Why are you do that?";
-            document.getElementById('img-Bill1').style.display='block';
+            showMessage1("😡Why are you do that?");
         }
      
     }
     else{
-        document.getElementById('error-message1').style.display ="block";
-        message1.innerText = "😡You test my code?";
-        document.getElementById('img-Bill1').style.display = 'block';
+        showMessage1("😡You test my code?");
     }
     
 });
@@ -52,22 +49,23 @@ checkbutton.addEventListener("click" ,function validateBillAndCashAmount(){
         hideMessage();
         if(isNumeric(cashGiven.value))
         {
-            if(cashGiven.value >= billAmount.value)
+            if(cashGiven.value  >= billAmount.value)
             {   
                 const amountToBeReturned = cashGiven.value - billAmount.value;
-                calculateChange(amountToBeReturned);
+                calculateChange(amountToBeReturned); 
                 document.getElementById('change-table').style.display = 'block';
+
             }
             else{
-                showMessage("Do you wanna wash plates?");
+                showMessage2("Do you wanna wash plates?");
+                document.getElementById('img-err3').style.display='block';
             }
          
         }
         else{
             document.getElementById('change-table').style.display="none";
-            document.getElementById('error-message2').style.display ="block";
-            message2.innerText = "😡why you do again?";
-            document.getElementById('img-Bill2').style.display = 'block';
+            showMessage2("Happy??"); 
+            document.getElementById('img-err2').style.display = 'block';
         }
     
    
@@ -76,7 +74,7 @@ checkbutton.addEventListener("click" ,function validateBillAndCashAmount(){
 
 function hideMessage(){
     message2.style.display ="none";
-    document.getElementById('img-Bill2').style.display = 'none';
+    document.getElementById('img-err2').style.display = 'none';
 }
 function calculateChange(amountToBeReturned)
 {
@@ -86,7 +84,12 @@ function calculateChange(amountToBeReturned)
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
-function showMessage(msg){
+function showMessage1(msg){
+    document.getElementById('error-message1').style.display ="block";
+    message1.innerText = msg;
+    document.getElementById('img-err1').style.display = 'block';
+}
+function showMessage2(msg){
     message2.style.display ="block";
     message2.innerText = msg;
 }
